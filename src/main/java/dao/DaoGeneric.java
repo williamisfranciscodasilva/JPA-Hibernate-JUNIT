d7e2bf6f0761238d51148c384ce38153242fc8e1
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-04-02 07:44:49 -0300

    criando o DAO, e inserindo dados no banco

---
+++ src/main/java/dao/DaoGeneric.java
@@ -0,0 +1,16 @@
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import posjavamavenhibernate.HibernateUtil;
+
+public class DaoGeneric<E> {
+	private EntityManager entityManager = HibernateUtil.getEntityManager();
+	public void salvar(E entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(entidade);
+		transaction.commit();
+	}
+}
--- src/test/java/posjavamavenhibernate/HibernateUtilTest.java
+++ src/test/java/posjavamavenhibernate/HibernateUtilTest.java
@@ -2,9 +2,21 @@ package posjavamavenhibernate;
 
 import org.junit.Test;
 
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
 public class HibernateUtilTest {
 	@Test
 	public void testeHibernateUtil() {
-		HibernateUtil.getEntityManager();
+		//HibernateUtil.getEntityManager();
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		pessoa.setIdade(34);
+		pessoa.setLogin("teste6");
+		pessoa.setNome("Ana Paula da Silva");
+		pessoa.setSobrenome("Lins");
+		pessoa.setEmail("apaulaslins2@gmail.com");
+		pessoa.setSenha("789");
+		daoGeneric.salvar(pessoa);
 	}
 }
