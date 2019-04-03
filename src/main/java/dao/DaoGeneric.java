00e69b7efd105ef146a1d9dc63943564b5f15b7c
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-04-03 07:42:03 -0300

    criando update/atualizar com hibernate

--- src/main/java/dao/DaoGeneric.java
+++ src/main/java/dao/DaoGeneric.java
@@ -3,6 +3,7 @@ package dao;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import model.UsuarioPessoa;
 import posjavamavenhibernate.HibernateUtil;
 
 public class DaoGeneric<E> {
@@ -16,6 +17,17 @@ public class DaoGeneric<E> {
 	public E pesquisar(E entidade) {
 		Object id = HibernateUtil.getPrimaryKey(entidade);
 		E e = (E) entityManager.find(entidade.getClass(), id);
+		return e;
+	}
+	public E updateMerge(E entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		E entidadeSalva = entityManager.merge(entidade);
+		transaction.commit();
+		return entidadeSalva;
+	}
+	public E pesquisar2(long id, Class<E> entidade) {
+		E e = (E) entityManager.find(entidade, id);
 		return e;
 	}
 }
--- src/test/java/posjavamavenhibernate/HibernateUtilTest.java
+++ src/test/java/posjavamavenhibernate/HibernateUtilTest.java
@@ -6,7 +6,7 @@ import dao.DaoGeneric;
 import model.UsuarioPessoa;
 
 public class HibernateUtilTest {
-	@Test
+/*	@Test
 	public void testeHibernateUtil() {
 		//HibernateUtil.getEntityManager();
 		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
@@ -18,13 +18,22 @@ public class HibernateUtilTest {
 		pessoa.setEmail("apaulaslins2@gmail.com");
 		pessoa.setSenha("789");
 		daoGeneric.salvar(pessoa);
-	}
-	@Test
+	}*/
+	/*@Test
 	public void testeBuscar() {
 		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
 		UsuarioPessoa pessoa = new UsuarioPessoa();
 		pessoa.setId(4L);
 		pessoa = daoGeneric.pesquisar(pessoa);
+		System.out.println(pessoa);
+	}*/
+	@Test
+	public void testeUpdate() {
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		UsuarioPessoa pessoa = daoGeneric.pesquisar2(2L, UsuarioPessoa.class);
+		pessoa.setIdade(88);
+		pessoa.setNome("Nome atualizado no hibernate");
+		pessoa = daoGeneric.updateMerge(pessoa);
 		System.out.println(pessoa);
 	}
 }
